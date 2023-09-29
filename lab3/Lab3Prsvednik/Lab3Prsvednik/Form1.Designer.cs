0acfca7eddfde8945b90cbc763b261317c8b06fd
PrAl4 <74269251+PrAl4@users.noreply.github.com>
2023-09-29 17:06:29 +0300

    Add files via upload

---
+++ lab3/Lab3Prsvednik/Lab3Prsvednik/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Lab3Prsvednik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(989, 382);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MD);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MU);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MMove);
+           // this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MMove);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(762, 425);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 425);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(97, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Bresenham";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 451);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "VU";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 503);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+
+    }
+}
+
---
+++ lab3/Lab3Prsvednik/Lab3Prsvednik/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3Prsvednik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            gr.Clear(Color.White);
+
+        }
+
+
+        static Bitmap bmp = new Bitmap(989, 382);
+
+        public Graphics gr = Graphics.FromImage(bmp);
+        Pen p = new Pen(Color.Black);
+        bool Mose_D = false;
+        public int x0, y0;
+
+        void MD(object sen, MouseEventArgs e)
+        {
+            Mose_D = true;
+            x0 = (int)e.X;
+            y0 = (int)e.Y;
+        }
+
+        void MU(object sen, MouseEventArgs e) { Mose_D = false; }
+
+        void MMove(object sen, MouseEventArgs e)
+        {
+            if (Mose_D)
+            {
+                if (radioButton1.Checked)
+                {
+                    Algorithm_Bras(x0, y0, e.X, e.Y);
+                }
+                else if (radioButton2.Checked)
+                {
+                    Algorithm_Vu(x0, y0, e.X, e.Y);
+                }
+            }
+        }
+
+        void DrawVu(int x, int y, float transparency)
+        {
+            Color c = bmp.GetPixel(x, y);
+            bmp.SetPixel(x, y, Color.FromArgb(255, (int)(c.R * (1 - transparency)), (int)(c.G * (1 - transparency)), (int)(c.B * (1 - transparency))));
+        }
+
+        void Algorithm_Bras(int x0, int y0, int x1, int y1)
+        {
+            int dX = Math.Abs(x1 - x0), dY = -Math.Abs(y1 - y0);
+            int sx, sy;
+            if (x0 < x1)
+                sx = 1;
+            else
+            {
+                sx = -1;
+            }
+            if (y0 < y1)
+                sy = 1;
+            else
+            {
+                sy = -1;
+            }
+            int err = dX + dY;
+            bmp.SetPixel(x1, y1, Color.Black);
+            while (x0 != x1 || y0 != y1)
+            {
+                bmp.SetPixel(x0, y0, Color.Black);
+                int err2 = err * 2;
+                if (err2 >= dY)
+                {
+                    err += dY;
+                    x0 += sx;
+                }
+                if (err2 <= dX)
+                {
+                    err += dX;
+                    y0 += sy;
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        void Algorithm_Vu(int x0, int y0, int x1, int y1)
+        {
+            int xt0 = x0, yt0 = y0, xt1 = x1, yt1 = y1;
+            int temp1, temp2;
+            if (Math.Abs(x1 - x0) < Math.Abs(y1 - y0))
+            {
+                temp1 = x0;
+                temp2 = x1;
+                x0 = y0;
+                y0 = temp1;
+                x1 = y1;
+                y1 = temp2;
+            }
+            if (x0 > x1)
+            {
+                temp1 = x0;
+                temp2 = y0;
+                x0 = x1;
+                x1 = temp1;
+                y0 = y1;
+                y1 = temp2;
+            }
+            float dx = x1 - x0, dy = y1 - y0, tranc = dy / dx, y = y0 + tranc;
+            for (var i = x0 + 1; i < x1 - 1; i++)
+            {
+                if (Math.Abs(yt1 - yt0) > Math.Abs(xt1 - xt0))
+                {
+                    DrawVu((int)(y), i, 1 - (y - (int)y));
+                    DrawVu((int)(y) + 1, i, 1 - (y - (int)y));
+                }
+                else
+                {
+                    DrawVu(i, (int)y, 1 - (y - (int)y));
+                    DrawVu(i, (int)y + 1, 1 - (y - (int)y));
+                }
+                y += tranc;
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gr.Clear(Color.White);
+            pictureBox1.Image = null;
+        }
+    }
+}
