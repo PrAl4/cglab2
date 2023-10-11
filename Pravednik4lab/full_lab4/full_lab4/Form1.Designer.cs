dd03a0b5cd76abf012fba9015e561f1ff9d79e0b
PrAl4 <74269251+PrAl4@users.noreply.github.com>
2023-10-11 19:30:47 +0300

    Add files via upload
    Праведник. Примитивы и проверки

---
+++ Pravednik4lab/full_lab4/full_lab4/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace full_lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Draw = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.line = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.TestPoint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(313, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(847, 557);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MD);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MU);
+            // 
+            // Draw
+            // 
+            this.Draw.Location = new System.Drawing.Point(12, 76);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(132, 57);
+            this.Draw.TabIndex = 1;
+            this.Draw.Text = "Нарисовать полигон";
+            this.Draw.UseVisualStyleBackColor = true;
+            this.Draw.Click += new System.EventHandler(this.Draw_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 368);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(215, 20);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = " Выпуклому многоугольнику";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 394);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(229, 20);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Невыпуклому многоугольнику";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 447);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(272, 20);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Классифицировать положение точки";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // line
+            // 
+            this.line.Location = new System.Drawing.Point(12, 139);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(132, 58);
+            this.line.TabIndex = 6;
+            this.line.Text = "Нарисовать линию";
+            this.line.UseVisualStyleBackColor = true;
+            this.line.Click += new System.EventHandler(this.line_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Проверить, принадлежит ли:";
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(12, 12);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(132, 58);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // TestPoint
+            // 
+            this.TestPoint.Location = new System.Drawing.Point(12, 276);
+            this.TestPoint.Name = "TestPoint";
+            this.TestPoint.Size = new System.Drawing.Size(132, 58);
+            this.TestPoint.TabIndex = 8;
+            this.TestPoint.Text = "Проверить точку";
+            this.TestPoint.UseVisualStyleBackColor = true;
+            this.TestPoint.Click += new System.EventHandler(this.TestPoint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 581);
+            this.Controls.Add(this.TestPoint);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.line);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Draw);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Draw;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button line;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button TestPoint;
+    }
+}
+
---
+++ Pravednik4lab/full_lab4/full_lab4/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace full_lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gr.Clear(Color.White);
+        }
+        static Bitmap bmp = new Bitmap(1000, 400);
+
+        public Graphics gr = Graphics.FromImage(bmp);
+
+        List<Point> points = new List<Point>();
+        List<Point> lines = new List<Point>();
+        bool Mose_D = false;
+        public int x0, y0;
+
+        void MD(object sen, MouseEventArgs e)
+        {
+            points.Add(new Point(e.X, e.Y));
+        }
+
+        void MU(object sen, MouseEventArgs e) { Mose_D = false; }
+
+        void MouseClick(object sen, MouseEventArgs e)
+        {
+            if(Mose_D)
+                points.Add(new Point(e.X, e.Y));
+        }
+
+        void Algorithm_Bras(Point p1, Point p2)
+        {
+            int x0 = (int)p1.X, y0 = (int)p1.Y, y1 = (int)p2.Y, x1 = (int)p2.X;
+            int dX = Math.Abs(x1 - x0), dY = -Math.Abs(y1 - y0);
+            int sx, sy;
+            if (x0 < x1)
+                sx = 1;
+            else
+            {
+                sx = -1;
+            }
+            if (y0 < y1)
+                sy = 1;
+            else
+            {
+                sy = -1;
+            }
+            int err = dX + dY;
+            bmp.SetPixel(x1, y1, Color.Black);
+            while (x0 != x1 || y0 != y1)
+            {
+                bmp.SetPixel(x0, y0, Color.Black);
+                int err2 = err * 2;
+                if (err2 >= dY)
+                {
+                    err += dY;
+                    x0 += sx;
+                }
+                if (err2 <= dX)
+                {
+                    err += dX;
+                    y0 += sy;
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            gr.Clear(Color.White);
+            pictureBox1.Image = null;
+            points.Clear();
+            lines.Clear();
+        }
+
+        private void line_Click(object sender, EventArgs e)
+        {
+            if (points.Count == 2)
+            {
+                Algorithm_Bras(points[0], points[1]);
+                lines.Add(points[0]);
+                lines.Add(points[1]);
+                points.Clear();
+            }
+            else if(points.Count < 2)
+            {
+               MessageBox.Show("Точек недостаточно для построения линии.");
+            }else if(points.Count > 2)
+            {
+                MessageBox.Show("Точек больше чем две. Попробуйте нарисовать полигон");
+            }
+        }
+
+        private void Draw_Click(object sender, EventArgs e)
+        {
+            if (points.Count > 2)
+            {
+                for (int i = 0; i < points.Count() - 1; i++)
+                {
+                    Algorithm_Bras(points[i], points[i + 1]);
+                    lines.Add(points[i]);
+                    lines.Add(points[i+1]);
+                }
+                if (points[0] != points[points.Count - 1])
+                    Algorithm_Bras(points[points.Count - 1], points[0]);
+                points.Clear();
+            }
+            else if (points.Count == 2)
+            {
+                MessageBox.Show("Точек недостаточно для построения полигона. Попробуйте построить линию");
+            }else if (points.Count < 2)
+            {
+                MessageBox.Show("Точек недостаточно для построения линии или полигона.");
+            }
+        }
+
+        void InPolygon1()
+        {
+            int j = lines.Count - 1;
+            bool flag = false;
+            for(int i = 0; i < lines.Count(); i++)
+            {
+                if (lines[i].Y < points[0].Y && lines[j].Y >= points[0].Y
+                    || lines[j].Y < points[0].Y && lines[i].Y >= points[0].Y)
+                {
+                    if (lines[i].X + (points[0].Y - lines[i].Y) / (lines[j].Y - lines[i].Y)
+                        * (lines[j].X - lines[i].X) < points[0].X)
+                    {
+                        flag = !flag;
+                    }
+                }
+                j = i;
+            }
+            if (flag)
+            {
+                MessageBox.Show("Точка принадлежит невыпуклому многоугольнику");
+            }
+            else
+            {
+                MessageBox.Show("Точка не принадлежит невыпуклому многоугольнику");
+            }
+        }
+
+        void InPolygon2()
+        {
+            int j = lines.Count - 1;
+            bool flag = false;
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                if (lines[i].Y < points[0].Y && lines[j].Y >= points[0].Y
+                    || lines[j].Y < points[0].Y && lines[i].Y >= points[0].Y)
+                {
+                    if (lines[i].X + (points[0].Y - lines[i].Y) / (lines[j].Y - lines[i].Y)
+                        * (lines[j].X - lines[i].X) < points[0].X)
+                    {
+                        flag = !flag;
+                    }
+                }
+                j = i;
+            }
+            if (flag)
+            {
+                MessageBox.Show("Точка принадлежит выпуклому многоугольнику");
+            }
+            else
+            {
+                MessageBox.Show("Точка не принадлежит выпуклому многоугольнику");
+            }
+        }
+
+        void Position_Relative_Line()
+        {
+            if (((points[0].X - lines[0].X) * (lines[1].Y - lines[0].Y) -
+                (points[0].Y - lines[0].Y) * (lines[1].X - lines[0].X)) > 0) {
+                MessageBox.Show("Слева");
+            }else if (((points[0].X - lines[0].X) * (lines[1].Y - lines[0].Y) -
+                (points[0].Y - lines[0].Y) * (lines[1].X - lines[0].X)) < 0)
+            {
+                MessageBox.Show("Справа");
+            }
+            else
+            {
+                MessageBox.Show("На линии");
+            }
+        }
+
+        private void TestPoint_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                InPolygon2();
+                points.Clear();
+            } 
+            if (radioButton2.Checked)
+            {
+                InPolygon1();
+                points.Clear();
+            }
+            if (radioButton3.Checked)
+            {
+                Position_Relative_Line();
+                points.Clear();
+            }
+        }
+    }
+}
