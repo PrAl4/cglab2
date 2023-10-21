c77593e5273702774c5ad9206a7419c14e6de7e3
PrAl4 <74269251+PrAl4@users.noreply.github.com>
2023-10-21 20:23:58 +0300

    Add files via upload
    Праведник. Второй номер лабораторной 2

---
+++ pravednikLAB5Task2/pravednikLAB5Task2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿using System.Drawing;
+
+namespace pravednikLAB5Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Draw = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.dpth = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.rghnss = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(184, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(927, 555);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MD);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MU);
+            // 
+            // Draw
+            // 
+            this.Draw.Location = new System.Drawing.Point(11, 14);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(141, 65);
+            this.Draw.TabIndex = 1;
+            this.Draw.Text = "Нарисовать";
+            this.Draw.UseVisualStyleBackColor = true;
+            this.Draw.Click += new System.EventHandler(this.Draw_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(11, 101);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(141, 65);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // dpth
+            // 
+            this.dpth.Location = new System.Drawing.Point(11, 261);
+            this.dpth.Name = "dpth";
+            this.dpth.Size = new System.Drawing.Size(140, 22);
+            this.dpth.TabIndex = 4;
+            this.dpth.TextChanged += new System.EventHandler(this.dpth_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 221);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Глубина:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(11, 306);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Шероховатость:";
+            // 
+            // rghnss
+            // 
+            this.rghnss.Location = new System.Drawing.Point(11, 352);
+            this.rghnss.Name = "rghnss";
+            this.rghnss.Size = new System.Drawing.Size(140, 22);
+            this.rghnss.TabIndex = 6;
+            this.rghnss.TextChanged += new System.EventHandler(this.rghnss_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 566);
+            this.Controls.Add(this.rghnss);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dpth);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Draw);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Draw;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox dpth;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox rghnss;
+    }
+}
+
---
+++ pravednikLAB5Task2/pravednikLAB5Task2/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pravednikLAB5Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gr.Clear(Color.White);
+        }
+
+        class line
+        {
+            public Point leftP;
+            public Point rightP;
+            public
+            line(Point p1, Point p2)
+            {
+                leftP = p1;
+                rightP = p2;
+            }
+
+        };
+
+        static Bitmap bmp = new Bitmap(1000, 600);
+        public Graphics gr = Graphics.FromImage(bmp);
+        bool Mose_D = false;
+        List<Point> points = new List<Point>();
+        List<line> lines = new List<line>();
+        double roughness;
+        double depth;
+        string roughness1;
+        string depth1;
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        Pen Pen = new Pen(Color.Black, 3);
+        Pen Pen2 = new Pen(Color.Black, 1);
+        void MU(object sen, MouseEventArgs e) { Mose_D = false; }
+        void MD(object sen, MouseEventArgs e)
+        {
+            points.Add(new Point(e.X, e.Y));
+        }
+
+        void MouseClick(object sen, MouseEventArgs e)
+        {
+            if (Mose_D)
+            {
+                points.Add(e.Location);
+                if (points.Count == 2)
+                {
+                    lines.Add(new line(points[0], points[1]));
+                }
+                gr.FillEllipse(blackBrush, e.X - 3, e.Y - 3, 7, 7);
+            }
+        }
+
+        void DrawM()
+        {
+            depth = Double.Parse(depth1);
+            roughness = Double.Parse(roughness1);
+            for(int i = 1; i <= (int)depth; i++)
+            {
+                Random r = new Random();
+                List<line> drawp = new List<line>();
+                foreach(line x in lines)
+                {
+                    Point L = x.leftP;
+                    Point R = x.rightP;
+                    double j = Math.Abs(L.X - R.X) * Math.Abs(L.X - R.X) - Math.Abs(L.Y - R.Y) * Math.Abs(L.Y - R.Y);
+                    j = Math.Sqrt(j);
+                    int center = (L.X + R.X) / 2;
+                    double height = (L.Y + R.Y) / 2 + (r.NextDouble() - 0.5) * roughness * j;
+                    Point cen = new Point(center, (int)height);
+                    drawp.Add(new line(L, cen));
+                    drawp.Add(new line(cen, R));
+                }
+                lines = drawp;
+            }
+            foreach (line x in lines)
+                Algorithm_Bras(x.leftP, x.rightP);
+        }
+
+        private void Draw_Click(object sender, EventArgs e)
+        {
+            DrawM();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            gr.Clear(Color.White);
+            pictureBox1.Image = null;
+            points.Clear();
+            lines.Clear();
+        }
+
+        private void dpth_TextChanged(object sender, EventArgs e)
+        {
+
+            roughness1 = rghnss.Text;
+        }
+
+        private void rghnss_TextChanged(object sender, EventArgs e)
+        {
+            depth1 = dpth.Text;
+        }
+
+        void Algorithm_Bras(Point p1, Point p2)
+        {
+            int x0 = (int)p1.X, y0 = (int)p1.Y, y1 = (int)p2.Y, x1 = (int)p2.X;
+            int dX = Math.Abs(x1 - x0), dY = -Math.Abs(y1 - y0);
+            int sx, sy;
+            if (x0 < x1)
+                sx = 1;
+            else
+            {
+                sx = -1;
+            }
+            if (y0 < y1)
+                sy = 1;
+            else
+            {
+                sy = -1;
+            }
+            int err = dX + dY;
+            bmp.SetPixel(x1, y1, Color.Black);
+            while (x0 != x1 || y0 != y1)
+            {
+                bmp.SetPixel(x0, y0, Color.Black);
+                int err2 = err * 2;
+                if (err2 >= dY)
+                {
+                    err += dY;
+                    x0 += sx;
+                }
+                if (err2 <= dX)
+                {
+                    err += dX;
+                    y0 += sy;
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+    }
+}
